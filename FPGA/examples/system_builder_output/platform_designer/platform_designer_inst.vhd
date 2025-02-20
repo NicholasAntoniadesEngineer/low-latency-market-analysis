752775f015d95abd221c77aa6c4255c1f7657da3
Nicholas Antoniades <nickantoniades11@gmail.com>
2025-02-20 08:08:56 +0000

    platform designer empty framework

---
+++ FPGA/examples/system_builder_output/platform_designer/platform_designer_inst.vhd
@@ -0,0 +1,13 @@
+	component platform_designer is
+		port (
+			clk_clk       : in std_logic := 'X'; -- clk
+			reset_reset_n : in std_logic := 'X'  -- reset_n
+		);
+	end component platform_designer;
+
+	u0 : component platform_designer
+		port map (
+			clk_clk       => CONNECTED_TO_clk_clk,       --   clk.clk
+			reset_reset_n => CONNECTED_TO_reset_reset_n  -- reset.reset_n
+		);
+
